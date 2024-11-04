05b9fd7a1b2994943261ef9dc2846c6bb48dd3c6
ganhuyagdamdinnym <damdinnymg@gmail.com>
2024-11-05 04:28:14 +0800

    up

--- src/app/ethics/components/Slide1.tsx
+++ src/app/ethics/components/Slide1.tsx
@@ -1,9 +1,7 @@
 import Image from "next/image";
-
 export const Slide1 = () => {
   return (
-    <div className="h-full w-full overflow-auto flex flex-col lg:flex-row gap-4 justify-between items-center text-center bg-[#048c6b] rounded-lg p-8 lg:p-[100px] max-w-6xl mx-auto">
-      {/* Text Content */}
+    <div className="flex overflow-scroll flex-col-reverse gap-4 lg:flex-row  justify-between items-center text-center bg-[#048c6b] rounded-lg p-8 lg:p-[100px] w-full max-w-6xl">
       <div className="w-full lg:w-[400px] flex flex-col gap-4 items-center lg:items-start text-center lg:text-left">
         <h1 className="text-3xl lg:text-2xl font-extrabold text-white my-2">
           Олон төрлийн мэргэжлүүдийг ойлгохын ач холбогдол
@@ -17,13 +15,14 @@ export const Slide1 = () => {
           шийдвэр гаргаж, өөрийгөө хөгжүүлэх боломжтой.
         </p>
 
-        <button className="bg-[#00765a] w-40 text-white font-semibold px-6 py-2 rounded-full hover:bg-green-400">
+        <button
+          //   onClick={() => router.push("/ethics/slides")}
+          className="bg-[#00765a] w-40 text-white font-semibold px-6 py-2 rounded-full hover:bg-green-400"
+        >
           Start Slide
         </button>
       </div>
-
-      {/* Image Container */}
-      <div className="relative w-48 h-48 lg:w-96 lg:h-96 rounded-full overflow-hidden border-2 lg:border-[8px] border-white">
+      <div className="relative w-48 h-48 border-2 lg:w-96 lg:h-96 mb-4 rounded-full overflow-hidden lg:border-[8px] border-white">
         <Image
           src="/jobSearch.png"
           alt="Team Working"
